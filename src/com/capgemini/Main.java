ac1738164f40e8a13afafed2f8a4612b51463d0d
Victor <Victor.Tenbergen@capgemini.com>
2017-09-12 15:06:29 +0200

    Hello world test

--- src/com/capgemini/Main.java
+++ src/com/capgemini/Main.java
@@ -3,6 +3,8 @@ package com.capgemini;
 public class Main {
 
     public static void main(String[] args) {
+        String s = "Hello anyone who reads this!";
+        System.out.println(s);
 	// write your code here
     }
 }
