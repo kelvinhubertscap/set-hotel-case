344743504d6fdae7ecb084b814dd500df6346938
dburger <mail@duncanburger.nl>
2017-09-19 12:57:44 +0200

    7.1 7.2 8.1

--- src/main/java/com/capgemini/core/Tour.java
+++ src/main/java/com/capgemini/core/Tour.java
@@ -21,7 +21,8 @@ public class Tour {
     public  final   int             tourId;
     public          LocalDateTime   startTime;
     private         LocalDateTime   endTime;
-    public          enum            TourType { R, M };
+    public          TourManager     tourManager;
+   // public          enum            TourType { R, M };
 
     public Tour(int tourId) {
         this.tourId     = tourId;
