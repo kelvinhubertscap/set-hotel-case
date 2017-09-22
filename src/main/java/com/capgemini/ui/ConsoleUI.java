91a6bad13fde4ff698faf5553129cd502326ee1a
dburger <mail@duncanburger.nl>
2017-09-22 12:48:44 +0200

    Userstory 7 af.3

--- src/main/java/com/capgemini/ui/ConsoleUI.java
+++ src/main/java/com/capgemini/ui/ConsoleUI.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static com.capgemini.core.TourType.parseTourType;
 
 public class ConsoleUI {
     /**
